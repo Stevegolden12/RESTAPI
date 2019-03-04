b744c6f85dfa83234666fb510f64f3835463cf29
Steve Golden <sgolden2525@outlook.com>
2019-03-03 23:53:20 -0600

    Install express
    Installed Express.js via npm.
    
    Added Public folder and index.html within.
    
    Added Express within index.js
    Added middleware to use static site
    Created server by app.listen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+let express = require('express');
+let app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, ()=>console.log(`Server has started on ${PORT}`));
