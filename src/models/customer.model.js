558ad2398b776768e046e729e6934d010e2ee403
Steve Golden <sgolden2525@outlook.com>
2019-03-16 21:21:55 -0500

    Fixes
    Add Mongoose via npm
    Add link from customer.model to index.js
    Change server information.

---
+++ src/models/customer.model.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+
+const server = 'cluster0-shard-00-01-pgooz.gcp.mongodb.net:27017';
+const database = 'cluster0';
+const user = 'User1';
+const password = 'User1';
+
+mongoose.connect('mongodb+srv://User1:<password>@cluster0-pgooz.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+
+let CustomerSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+
+module.exports = mongoose.model('Customer', CustomerSchema);
